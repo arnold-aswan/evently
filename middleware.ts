8118fe42c43db82ee1a4fb0863447767b0e14671
arnold-aswan <fredyaswan@gmail.com>
2024-10-12 13:47:26 +0300

    reconfigured  clerk middleware

--- middleware.ts
+++ middleware.ts
@@ -1,16 +1,13 @@
 import { clerkMiddleware } from "@clerk/nextjs/server";
 
-export default clerkMiddleware({
-    publicRoutes: [
-        "/", "/events/:id", "/api/webhook/clerk", "/api/uploadthing"
-    ],
-    ignoredRoutes: [
-        "/api/webhook/clerk", "/api/uploadthing"
-    ]
-});
+export default clerkMiddleware();
 
 export const config = {
   matcher: [
+    "/",
+    "/events/:id",
+    "/api/webhook/clerk",
+    "/api/uploadthing",
     // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
     // Always run for API routes
