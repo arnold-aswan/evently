cd8b2547c9a35eed71fb019f03141ed04a4bd029
arnold-aswan <fredyaswan@gmail.com>
2024-10-27 14:39:32 +0300

    added private routes

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,35 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher([
-  "/",
-  "/sign-in",
-  "/sign-up", 
-  "/events/:id",
-  "/api/webhook/clerk",
-  "/api/uploadthing",
-])
+// const isPublicRoute = createRouteMatcher([
+//   "/",
+//   "/sign-in",
+//   "/sign-up", 
+//   "/events/:id",
+//   "/api/webhook/clerk",
+//   "/api/uploadthing",
+// ])
+
+// Define the private routes
+const isPrivateRoute = createRouteMatcher([
+  "/dashboard",       
+  "/profile",         
+  "/events/create",  
+]);
 
 export default clerkMiddleware((auth, req) => {
   console.log("Request URL:", req.url); // Log to inspect
   const { userId } = auth();
 
-  if (isPublicRoute(req)) {
-    return; // Allow public routes without authentication
-  }
-
-  if (!userId) {
-    return new Response("Unauthorized", {
-      status: 302,
-      headers: {
-        Location: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL || '/sign-in',
-      },
-    });
+  if (isPrivateRoute(req)) {
+    if (!userId) {
+      // Redirect to sign-in if not authenticated
+      return new Response("Unauthorized", {
+        status: 302,
+        headers: {
+          Location: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL || '/sign-in',
+        },
+      });
+    }
   }
 });
 
