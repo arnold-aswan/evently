773d8436b48a47141c394af8cd646a2c74c9108e
arnold-aswan <fredyaswan@gmail.com>
2024-10-27 14:31:44 +0300

    updated routes to public routes

--- middleware.ts
+++ middleware.ts
@@ -2,17 +2,31 @@ import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
+  "/sign-in",
+  "/sign-up", 
   "/events/:id",
   "/api/webhook/clerk",
   "/api/uploadthing",
 ])
 
-export default clerkMiddleware(async (auth, req) => {
+export default clerkMiddleware((auth, req) => {
+  console.log("Request URL:", req.url); // Log to inspect
+  const { userId } = auth();
+
   if (isPublicRoute(req)) {
-    return 
+    return; // Allow public routes without authentication
   }
-  await auth().protect()
+
+  if (!userId) {
+    return new Response("Unauthorized", {
+      status: 302,
+      headers: {
+        Location: process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL || '/sign-in',
+      },
+    });
+  }
 });
+
 
 export const config = {
   matcher: [
